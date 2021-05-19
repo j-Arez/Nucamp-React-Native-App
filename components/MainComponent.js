7d007d5eee6540c5d3ddc686d42eaadff1261d2e
Jonathan Almendarez <jonsaa13110@gmail.com>
2021-05-19 16:05:57 -0400

    Issues connecting to store

--- components/MainComponent.js
+++ components/MainComponent.js
@@ -11,6 +11,19 @@ import Contact from './ContactComponent';
 import { Icon } from 'react-native-elements';
 import SafeAreaView from 'react-native-safe-area-view';
 import ExpoStatusBar from 'expo-status-bar/build/ExpoStatusBar';
+import { connect } from 'react-redux';
+import {
+    fetchCampsites, fetchComments, fetchPromotions,
+    fetchPartners
+} from '../redux/ActionCreators';
+
+
+const mapDispatchToProps = {
+    fetchCampsites,
+    fetchComments,
+    fetchPromotions,
+    fetchPartners
+};
 
 const CustomDrawerContentComponent = props => (
     <ScrollView>
@@ -197,6 +210,13 @@ const AppNavigator = createAppContainer(MainNavigator);
 
 class Main extends Component {
 
+    componentDidMount() {
+        this.props.fetchCampsites();
+        this.props.fetchComments();
+        this.props.fetchPartners();
+        this.props.fetchPromotions();
+    }
+
     render() {
         return (
             <View style={{
@@ -237,4 +257,4 @@ const styles = StyleSheet.create({
     }
 });
 
-export default Main;
+export default connect(null, mapDispatchToProps)(Main);
