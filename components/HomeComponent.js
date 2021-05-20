ba7f5a22d834fc31e278a4a41fb371f6cb45c30e
Jonathan Almendarez <jonsaa13110@gmail.com>
2021-05-20 12:39:49 -0400

    Added activity indicator

--- components/HomeComponent.js
+++ components/HomeComponent.js
@@ -3,6 +3,7 @@ import { Card } from 'react-native-elements';
 import { Text, ScrollView, View } from 'react-native';
 import { connect } from 'react-redux';
 import { baseUrl } from '../shared/baseUrl';
+import Loading from './LoadingComponent';
 
 const mapStateToProps = state => {
     return {
@@ -12,7 +13,19 @@ const mapStateToProps = state => {
     };
 };
 
-function RenderItem({ item }) {
+function RenderItem(props) {
+    const { item } = props;
+
+    if (props.isLoading) {
+        return <Loading />
+    }
+    if (props.errMess) {
+        return (
+            <View>
+                <Text>{props.errMess}</Text>
+            </View>
+        );
+    }
     if (item) {
         return (
             <Card featuredTitle={item.name}
@@ -35,9 +48,21 @@ class Home extends Component {
     render() {
         return (
             <ScrollView>
-                <RenderItem item={this.props.campsites.campsites.filter(campsite => campsite.featured)[0]} />
-                <RenderItem item={this.props.promotions.promotions.filter(promotion => promotion.featured)[0]} />
-                <RenderItem item={this.props.partners.partners.filter(partner => partner.featured)[0]} />
+                <RenderItem
+                    item={this.props.campsites.campsites.filter(campsite => campsite.featured)[0]}
+                    isLoading={this.props.campsites.isLoading}
+                    errMess={this.props.campsites.errMess}
+                />
+                <RenderItem
+                    item={this.props.promotions.promotions.filter(promotion => promotion.featured)[0]}
+                    isLoading={this.props.promotions.isLoading}
+                    errMess={this.props.promotions.errMess}
+                />
+                <RenderItem
+                    item={this.props.partners.partners.filter(partner => partner.featured)[0]}
+                    isLoading={this.props.partners.isLoading}
+                    errMess={this.props.partners.errMess}
+                />
             </ScrollView>
         );
     }
